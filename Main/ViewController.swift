492af4bef925ec966da4121a7e2edc97797a8259
Garvin Casinillo <garvincasinillo@garvins-imac.local>
2017-01-06 09:23:20 -0500

    Initial Commit

---
+++ Main/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Main
+//
+//  Created by Garvin Casinillo on 1/6/17.
+//  Copyright © 2017 Garvin Casinillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
